ebb15bcf3f93202dbbe5f25f7a0b5c7e2fe9e116
rodmedeiross <roddmedeiross@gmail.com>
2021-08-15 21:15:10 -0300

    db Users relacion, foreingkey

--- src/database/migrations/1629046733034-CreateAppointments.ts
+++ src/database/migrations/1629046733034-CreateAppointments.ts
@@ -10,7 +10,7 @@ export default class CreateAppointments1629046733034
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: `uuid_generate_v4()`,
