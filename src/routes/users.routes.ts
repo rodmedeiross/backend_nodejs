d212ffb7165ffdf0ea90b05692ad40c7c666a8a2
rodmedeiross <roddmedeiross@gmail.com>
2021-08-19 23:43:49 -0300

    Upload File init

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticaded from '../middlewares/ensureAuthenticaded';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+const singleUploadAvatar = upload.single('avatar');
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +28,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticaded,
+  singleUploadAvatar,
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
